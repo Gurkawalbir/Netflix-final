9dbef6ee7ee2bb12415d4a517d94cbcb8f81f3cd
Gurkawalbir <57713328+Gurkawalbir@users.noreply.github.com>
2022-08-14 10:50:42 +0530

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import netflixLogo from "../img/netflixLogo.png";
+import netflixAvatar from "../img/netflixAvatar.png";
+
+const Navbar = () => {
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    handleScroll();
+  }, []);
+
+  const handleScroll = () => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setIsScrolling(true) : setIsScrolling(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  };
+
+  return (
+    <nav className={`Navbar ${isScrolling && "Navbar__black"}`}>
+      <img className="Navbar__logo" src={netflixLogo} alt="Netflix Logo" />
+      <img
+        className="Navbar__avatar"
+        src={netflixAvatar}
+        alt="Netflix Avatar"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
